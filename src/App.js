1569c0f5874297d4f006c4c3fb4a8ffad3e59686
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-07-16 18:55:19 +0100

    build the structure and logic of the app, and start styling it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  // setting the hooks
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
+
+  const addToDoItems = () => {
+    const inputItem = inputRef.current.value;
+    setTodos([...todos, inputItem]);
+    inputItem.current.value = "";
+  }
+
+  // the web page structure
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To Do List</h2>
+      <div className='to-do-container'>
+        <ul>
+          {
+            todos.map((item) => {
+              return <li>{item}</li>;
+            })
+          }
+        </ul>
+        <input ref={inputRef} placeholder='Enter an Item...'/>
+        <button onClick={addToDoItems}>Add</button>
+      </div>
     </div>
   );
 }
