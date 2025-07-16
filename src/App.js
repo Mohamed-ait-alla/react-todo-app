f701fd9489ac6d93d26bd7e4adfb4488c0071ad0
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-07-16 21:21:09 +0100

    Improve the app logic and start handling the removing action

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ function App() {
   const [todos, setTodos] = useState([]);
   const inputRef = useRef();
 
+  // function that addes items to the list
   const addToDoItems = () => {
     const inputItem = inputRef.current.value;
-    setTodos([...todos, inputItem]);
-    inputItem.current.value = "";
+    const newItem = {completed: false, inputItem};
+    setTodos([...todos, newItem]);
+    inputRef.current.value = "";
   }
 
+  // function that handles successfully completed items
+  const handleDoneItems = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+    }
+  
   // the web page structure
   return (
     <div className="App">
@@ -19,8 +28,8 @@ function App() {
       <div className='to-do-container'>
         <ul>
           {
-            todos.map((item) => {
-              return <li>{item}</li>;
+            todos.map((item, index) => {
+              return <li key = {index + Math.random() * 5} className= {item.completed ? "done": ""} onClick={() => handleDoneItems(index)}>{item.inputItem}</li>;
             })
           }
         </ul>
